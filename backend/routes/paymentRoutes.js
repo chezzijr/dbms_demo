f0b97fc9c5fa5aa7c7a481278aece6d1674009ee
QuocHuy1202 <huy.huynh120204@hmail.com>
2024-12-10 10:47:52 +0700

    Remove backend

--- backend/routes/paymentRoutes.js
+++
@@ -1,9 +0,0 @@
-// routes/paymentRoutes.js
-const express = require('express');
-const paymentController = require('../controllers/paymentControllers');
-const router = express.Router();
-
-// Định nghĩa route cho API lấy dữ liệu thanh toán
-router.get('/get', paymentController.getPaymentsInRange);
-router.get('/revenue', paymentController.calculateTotalRevenueInRange);
-module.exports = router;
