51dd32c8144a9ce03800e88eb69fbc5cd4aa16ae
thuanngza2203 <133408204+thuanngza2203@users.noreply.github.com>
2024-12-11 19:23:32 +0700

    finish-demo

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,21 @@
 // src/App.js
 import React from "react";
+import AddStudentForm from "./pages/addStudent.jsx";
+import StudentForm from "./pages/StudentForm.jsx";
+import DeleteStudent from "./pages/deleteStudent.jsx";  // Import DeleteStudent
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { Revenue } from "./pages/revenue.jsx";  // Import component TrangCh
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import { Revenue } from "./pages/revenue.jsx";
+
 function App() {
   return (
-    
-      <Router>
-          <Routes>
-            <Route path="/revenue" element={< Revenue />} /> 
-          </Routes>
-      </Router>
-    
+    <Router>
+      <Routes>
+        <Route path="/add" element={<AddStudentForm />} />
+        <Route path="/student" element={<StudentForm />} />
+        <Route path="/delete" element={<DeleteStudent />} /> 
+        <Route path="/revenue" element={<Revenue/>}/>
+      </Routes>
+    </Router>
   );
 }
 
